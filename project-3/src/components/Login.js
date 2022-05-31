b8f6a4c1a2fb1c76de9536d426e1f5634e2091a4
Brooke Love <savienloveb@gmail.com>
2022-05-31 14:07:03 -0700

    creating login page

--- project-3/src/components/Login.js
+++ project-3/src/components/Login.js
@@ -1,50 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
 import '../styles/Login.css'
-
+import Scroll from "../assets/images/600px\ scroll.png";
 // creating a functioning page
 
-class Login extends Component() {
-    componentDidMount() {
-        loginpage();
-    }
-    render () {
+export default function Login () {
         return (
-        <div>
-            <div class='box'>
-                    <div class='box-form'>
-                    <div class='box-login-tab'></div>
-                    <div class='box-login-title'>
-                        <div class='i i-login'></div><h2>LOGIN</h2>
-                    </div>
-                    <div class='box-login'>
-                        <div class='fieldset-body' id='login_form'>
-                            <button onclick="openLoginInfo();" class='b b-form i i-more' title='Mais Informações'></button>
-                            <p class='field'>
-                                <label for='user'>E-MAIL</label>
-                                <input type='text' id='user' name='user' title='Username' />
-                                <span id='valida' class='i i-warning'></span>
-                            </p>
-                            <p class='field'>
-                                <label for='pass'>PASSWORD</label>
-                                <input type='password' id='pass' name='pass' title='Password' />
-                                <span id='valida' class='i i-close'></span>
-                            </p>
-                            <input type='submit' id='do_login' value='GET STARTED' title='Get Started' />
-                        </div>
-                    </div>
+            <div className="loginContainer">
+                <div className="contentContainer">
+                <div className='scrollImage '></div>
+                    <h1>Login Page</h1>
+                    <p>Please Login/Signup to Battle!</p>
+                    <form action="#" method="post" id="loginOut">
+                    <p>Email</p>
+                    <input name="email" type="text" placeholder="Your Email Here"id="emailInput" required></input>
+                    <p>Password</p>
+                    <input name="password" type="text" placeholder="Your Password Here"id="passwordInput" required></input>
+                    <input type="submit" value="Send Message" id="form_button" className="submitBTN"/>
+                    <br></br>
+                    <a>Create New Account</a>
+                    </form>
                 </div>
-                <div class='box-info'>
-                    <button onclick="closeLoginInfo();" class='b b-info i i-left' title='Back to Sign In'></button>
-        
-                    <button onclick="" class='b-support' title='About Us?'> About Us</button>
-                    <button onclick="" class='b-support' title='Credits'> Credits</button>
-                    <div class='line-wh'></div>
-                    <button onclick="" class='b-cta' title='Sign up now!'> CREATE ACCOUNT</button>
-                </div>
-            </div><div class='icon-credits'>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a>, <a href="http://www.flaticon.com/authors/budi-tanrim" title="Budi Tanrim">Budi Tanrim</a> & <a href="http://www.flaticon.com/authors/nice-and-serious" title="Nice and Serious">Nice and Serious</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-        </div>
-    );
-    }
+            </div>
+        );
 }
-
-export default Login;
