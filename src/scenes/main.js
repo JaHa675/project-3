7d731b5d17a36a7437f7832fa31f27d973df1e13
Dahlia Graves <dahlialolagraves@gmail.com>
2022-06-09 16:26:01 -0700

    changed all of the battle backgrounds to be more flush, added in the cat bossgit add .git add .git add .git add .git add .

--- src/scenes/main.js
+++ src/scenes/main.js
@@ -215,10 +215,10 @@ class Mains extends Phaser.Scene {
             if (firstPlayHouse !== false) {
                 firstPlayHouse = false;
                  console.log("input A test",firstPlayHouse);
-                this.scene.start ('Houses')
+                this.scene.start ('House')
              } else if (houseBossDefeated === false && firstPlayHouse === false ) {
                  console.log(houseBossDefeated)
-                 this.scene.switch('Houses')
+                 this.scene.switch('House')
              } 
         };
         // eventsCenter.on('classSelect', function(playerChange){
@@ -232,18 +232,18 @@ class Mains extends Phaser.Scene {
 
 // console.log(dahliaBossDefeated);
 // this is how originally worked ====================================================
-// this.input.keyboard.on('keydown-A', () => {
-//             //   console.log(firstPlay, dahliaBossDefeated)
+this.input.keyboard.on('keydown-C', () => {
+            //   console.log(firstPlay, dahliaBossDefeated)
         
-//              if (firstPlayDahlia !== false) {
-//                 firstPlayDahlia = false;
-//                  console.log("input A test",firstPlayDahlia);
-//                 this.scene.start ('Dahlias')
-//              } else if (dahliaBossDefeated === false && firstPlayDahlia === false ) {
-//                  console.log(dahliaBossDefeated)
-//                  this.scene.switch('Dahlias')
-//              } 
-//          })
+             if (firstPlayCat !== false) {
+                firstPlayCat = false;
+                 console.log("input A test",firstPlayCat);
+                this.scene.start ('Cats')
+             } else if (catBossDefeated === false && firstPlayCat === false ) {
+                 console.log(dahliaBossDefeated)
+                 this.scene.switch('Cats')
+             } 
+         })
 // ======================================================================================
 
         // collider only takes in two parameters
