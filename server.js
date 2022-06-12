3b8355e015c7675354bc4d6e726ce55cb01fbd7d
Lucas Roman <lucas.e.roman@gmail.com>
2022-06-11 18:26:59 -0700

    staging file path change in server.js to build from public

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'project-3','public');
+const publicPath = path.join(__dirname, '..', 'project-3','build');
 
 
 console.log(publicPath);
@@ -10,8 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
-    
-    console.log(publicPath, "do you see me line 14");
+    console.log(publicPath, "do you see me");
 });
 app.listen(port, () => {
     console.log('Server is up!');
