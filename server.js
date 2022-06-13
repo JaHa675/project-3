3274fb4805d539282a7f4501d89fdc49a980cbd8
Lucas Roman <lucas.e.roman@gmail.com>
2022-06-13 09:47:33 -0700

    hailmary

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'project-3', 'build');
+const publicPath = path.join(__dirname,'client', 'build');
 
 
 console.log(publicPath);
