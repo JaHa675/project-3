789d77211ac1bc250e9464614815b7a173b978d4
Dahlia Graves <dahlialolagraves@gmail.com>
2022-06-22 14:22:53 -0700

    added in the navbar, it is currently refreshing the current page and not redirecting. It was redirecting but it would take the phaser game engine with it

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -2,10 +2,11 @@ import React from 'react';
 import '../../styles/Landing.css'
 import logo from "../../assets/images/Battle_Trail_Gaming_Logo__AdobeCreativeCloudExpress.gif"
 
+
 export default function Landing () {
     return (
         <div>
-            <div className="landingContainer">
+           <div className="landingContainer">
                 <div style={{height: "250px"}}>
                 <img src={logo} alt ="wait until the page loads" className="battleLogo"/>
                 </div>
