4f2fc1246d922618a25d78265c2a3b1dc55ad188
Dahlia Graves <dahlialolagraves@gmail.com>
2022-06-28 19:11:45 -0700

    trying to get an alert to show for the navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Alert} from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
-export default function NavBar() {
-    // function refreshPage() {
-    //     window.location.reload(false);
+function LoginAlert() {
+    const [show, setShow] = useState(true);
+    return(
+    <div>
+    <Alert show={show} variant="success">
+    <Alert.Heading>Exit the Game?!</Alert.Heading>
+    <p>
+     Saved data will be lost if you navigate out of game
+    </p>
+    <hr />
+    <div className="d-flex justify-content-end">
+      <Button onClick={() => setShow(false)} variant="outline-success">
+        Nevermind
+      </Button>
+      <Link to="/login" >To Login</Link>
+    </div>
+  </Alert>
+  {/* {!show && <Button onClick={() => setShow(true)}>Show Alert</Button>} */}
+  </div>
+  )
 
-    //   }
+  }
+
+export default function NavBar() {
+
     return (
         <div>
             <nav className="navbar navbar-dark bg-dark" >
@@ -17,17 +38,16 @@ export default function NavBar() {
                 </p>
                 }
                 { document.location.pathname !== "/login" && 
-                <p className="nav-item " >
-                    <Link to="/login" >Login</Link>
+                <p className="nav-item " ><Button  onClick={LoginAlert}>Login</Button> 
                 </p>
                 }
-                {
+                {   document.location.pathname !== "/main" &&
                     <p className="nav-item " >
                     <Link to="/main" >Game</Link>
                 </p>
                 }
                 { document.location.pathname !== "/about" && 
-                <p className="nav-item " >
+                <p className="nav-item ">
                     <Link to="/about" >About The Creators</Link>
                 </p>
                 }
